4b4f4dda7a2cb9b26fa4d1a992385fa06bc48a95
smk7758 <maxkovko@yahoo.co.jp>
2018-01-26 23:05:42 +0900

    ver: snapshot-2018-01-26
    うごかない。

--- src/com/github/smk7758/VideoCommunicater/Networks/Server.java
+++ src/com/github/smk7758/VideoCommunicater/Networks/Server.java
@@ -3,17 +3,18 @@ package com.github.smk7758.VideoCommunicater.Networks;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.BindException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 import com.github.smk7758.VideoCommunicater.Capture;
 import com.github.smk7758.VideoCommunicater.Main;
 import com.github.smk7758.VideoCommunicater.Screens.MainController;
 
 public class Server extends Thread implements Closeable {
-	ServerSocket server_socket = null;
-	Socket socket = null;
+	ServerSocketChannel server_socket = null;
+	SocketChannel socket = null;
 	short port = 0;
 	boolean accepted = false;
 	MainController mctr = null;
@@ -25,7 +26,8 @@ public class Server extends Thread implements Closeable {
 		this.setDaemon(true);
 		this.port = port;
 		try {
-			server_socket = new ServerSocket(port);
+			server_socket = ServerSocketChannel.open();
+			server_socket.socket().bind(new InetSocketAddress(port));
 		} catch (BindException e) {
 			System.err.println(e.getMessage() + System.lineSeparator());
 		} catch (IOException e) {
@@ -38,17 +40,21 @@ public class Server extends Thread implements Closeable {
 	public void run() {
 		try {
 			System.out.println("start run in Server");
-			if (server_socket == null) return;
+			if (server_socket == null)
+				return;
 			System.out.println("before accept");
+			if (!server_socket.isOpen() || !server_socket.socket().isBound())
+				return;
 			socket = server_socket.accept();
 			System.out.println("after accept");
 			accepted = true;
-			System.out.println("Adress from: " + socket.getInetAddress());
-			send = new Send(socket.getOutputStream());
+			Main.camera_number = Main.mctr.isCamEmpty() ? Main.mctr.getCam() : Main.camera_number;
+			System.out.println("Adress from: " + socket.socket().getRemoteSocketAddress());
+			send = new Send(socket.socket().getOutputStream());
 			capture = new Capture(Main.camera_number, send);
-			receive = new Receive(socket.getInputStream());
-			send.start();
+			receive = new Receive(socket.socket().getInputStream());
 			capture.start();
+			send.start();
 			receive.start();
 		} catch (SocketException ex) {
 			System.err.println("Server socket Closed.");
@@ -61,10 +67,15 @@ public class Server extends Thread implements Closeable {
 	@Override
 	public void close() {
 		try {
-			if (send != null) send.close();
-			if (receive != null) receive.close();
-			if (socket != null) socket.close();
-			if (server_socket != null) server_socket.close();
+			if (send != null)
+				send.close();
+			if (receive != null)
+				receive.close();
+			if (socket != null)
+				socket.close();
+			if (server_socket != null)
+				server_socket.close();
+			Main.server = null;
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
